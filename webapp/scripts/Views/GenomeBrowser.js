d6f3a0e735518e17490e81acd60fbe216a393ac7
Paul Vauterin <pvaut@gmail.com>
2013-04-29 14:33:08 +0100

    Filterbank & points in same channel

--- webapp/scripts/Views/GenomeBrowser.js
+++ webapp/scripts/Views/GenomeBrowser.js
@@ -10,6 +10,24 @@ define([DQXSCRQ(), DQXSC("Framework"), DQXSC("Controls"), DQXSC("Msg"), DQXSC("S
                 that.myPage = iPage;
                 that.registerView();
 
+                //List of components that can be drawn on the genome browser
+                that.plotComponents = [];
+                that.plotComponents.push({ id: 'variable1', filterbankFolder: 'Signif', filterbankID: 'pval', color: DQX.Color(0.0, 0.0, 0.8) });
+                that.plotComponents.push({ id: 'variable2', color: DQX.Color(0.0, 0.0, 0.8) });
+                that.plotComponents.push({ id: 'variable3', color: DQX.Color(0.0, 0.0, 0.8) });
+                that.plotComponents.push({ id: 'variable4', color: DQX.Color(0.0, 0.0, 0.8) });
+                that.plotComponents.push({ id: 'variable5', color: DQX.Color(0.0, 0.0, 0.8) });
+                that.plotComponents.push({ id: 'variable6', color: DQX.Color(0.0, 0.0, 0.8) });
+                that.plotComponents.push({ id: 'variable7', color: DQX.Color(0.0, 0.0, 0.8) });
+
+                //List of filterbanked summary elements to draw
+                var summaryComps = [];
+                summaryComps.push({ id: 'Max', color: DQX.Color(0.0, 0, 1.0), opacity: 1.0 });
+                summaryComps.push({ id: 'Q99', color: DQX.Color(1.0, 0, 0.0), opacity: 0.35 });
+                summaryComps.push({ id: 'Q95', color: DQX.Color(0.5, 0, 0.0), opacity: 0.35 });
+                summaryComps.push({ id: 'Q50', color: DQX.Color(0.0, 0, 0.0), opacity: 0.35 });
+
+
                 that.createFramework = function () {
                     this.frameLeft = that.getFrame().addMemberFrame(Framework.FrameGroupVert('settings', 0.01))
                         .setMargins(5).setDisplayTitle('settings group').setFixedSize(Framework.dimX, 380);
@@ -25,8 +43,8 @@ define([DQXSCRQ(), DQXSC("Framework"), DQXSC("Controls"), DQXSC("Msg"), DQXSC("S
 
                     var browserConfig = {
                         serverURL: serverUrl,
-//                        chromnrfield: 'chrom',
-                        chromoIdField: 'chrom',//set this to use chromosome id's
+                        //                        chromnrfield: 'chrom',
+                        chromoIdField: 'chrom', //set this to use chromosome id's
                         annotTableName: 'annotation_tbl',
                         viewID: 'GenomeBrowser',
                         database: MetaData.database,
@@ -43,6 +61,8 @@ define([DQXSCRQ(), DQXSC("Framework"), DQXSC("Controls"), DQXSC("Msg"), DQXSC("S
                     this.panelBrowser.MaxZoomFactX = 1.0 / 0.2;
                     this.panelBrowser.getNavigator().setMinScrollSize(0.0001);
 
+                    Msg.listen('', { type: 'ZoomFactorXChanged', id: this.panelBrowser.myID }, $.proxy(that.updateChannelVisibility, that));
+
                     //Annotation table has 'chrX' chromosome identifiers rather than numbers, so we translate them
                     this.panelBrowser.getAnnotationFetcher().translateChromoId = function (id) { return 'chr' + parseInt(id); }
 
@@ -53,16 +73,38 @@ define([DQXSCRQ(), DQXSC("Framework"), DQXSC("Controls"), DQXSC("Msg"), DQXSC("S
 
                     this.createSNPChannels();
 
-                    this.createProfileChannels();
+                    //                    this.createProfileChannels();
 
                     //Causes the browser to start with a sensible start region
                     var firstChromosome = MetaData.chromosomes[0].id;
                     this.panelBrowser.setChromosome(firstChromosome, true, false);
                     this.panelBrowser.setPostInitialiseHandler(function () {
-                        that.panelBrowser.showRegion(that.panelBrowser.getChromoID(1), 20000000, 100000);
+                        that.panelBrowser.showRegion(that.panelBrowser.getChromoID(1), 0, MetaData.chromosomes[0].len * 1E6);
                     });
 
                 };
+
+
+                that.updateChannelVisibility = function () { //updates the visibility status of the elements of the viewer
+                    var zoomfact = this.panelBrowser.getZoomFactorX();
+                    var showSNPPoints = (zoomfact >= 0.0015);
+                    if (showSNPPoints != this.showSNPPoints) {
+                        this.showSNPPoints = showSNPPoints;
+                        $.each(that.plotComponents, function (idx, comp) {
+                            var theChannel = that.panelBrowser.findChannel(comp.id);
+                            theChannel.modifyComponentActiveStatus(comp.id, showSNPPoints);
+                            if ('filterbankFolder' in comp) {
+                                var summaryID = comp.filterbankID;
+                                $.each(summaryComps, function (idx, summaryComp) {
+                                    var summCompID = summaryID + '_' + summaryComp.id;
+                                    var summComp = theChannel.findComponent(summCompID);
+                                    summComp.setColor(summaryComp.color, summaryComp.opacity * (showSNPPoints?0.2:1));
+                                });
+                            }
+                        });
+                        //that.panelBrowser.render();
+                    }
+                }
 
 
                 //Create the channels that show information for each individual SNP
@@ -72,40 +114,62 @@ define([DQXSCRQ(), DQXSC("Framework"), DQXSC("Controls"), DQXSC("Msg"), DQXSC("S
                     this.dataFetcherSNPs = new DataFetchers.Curve(serverUrl, MetaData.database, MetaData.tableSNPInfo, 'pos');
                     this.panelBrowser.addDataFetcher(this.dataFetcherSNPs);
 
+                    //Create data fetcher that will fetch the filterbanked data
+                    this.dataFetcherProfiles = new DataFetcherSummary.Fetcher(serverUrl, 1000, 1200);
+                    this.dataFetcherProfiles.translateChromoId = function (inp) { return 'chr' + parseInt(inp); }
+                    this.panelBrowser.addDataFetcher(this.dataFetcherProfiles);
+                    var summaryFolder = 'FilterBank';
+
+
                     //Make sure we fetch the SNP id from the table
                     this.dataFetcherSNPs.addFetchColumn("snpid", "String");
                     this.dataFetcherSNPs.activateFetchColumn("snpid");
 
 
-                    //Create a channel that will show the IHS values
-                    var theChannel = ChannelYVals.Channel('IHS', { minVal: 0, maxVal: +7 });
-                    theChannel.minDrawZoomFactX = 0.0015;
-                    theChannel.setTitle("IHS");
-                    theChannel.setHeight(200);
-                    this.panelBrowser.addChannel(theChannel, false);
 
-                    //Attach a custom tooltip creation function to the channel
-                    theChannel.getToolTipContent = function (compID, pointIndex) {
-                        var value = this.myComponents[compID].myfetcher.getColumnPoint(pointIndex, compID);
-                        if (value != null)
-                            return that.dataFetcherSNPs.getColumnPoint(pointIndex, 'snpid') + '; ' + compID + '= ' + value.toFixed(2);
-                        else
-                            return 'No value';
-                    }
 
                     //List of all components that will go into this channel
-                    var comps = [
-                    { id: 'minus_log10_fixed_effect_pvalue', color: DQX.Color(0.0, 0.0, 0.8) }
-                    ];
-
                     var controlsList = [];
 
-                    $.each(comps, function (idx, comp) {
+                    $.each(that.plotComponents, function (idx, comp) {
+
+                        //Create a channel that will show the IHS values
+                        var theChannel = ChannelYVals.Channel(comp.id, { minVal: 0, maxVal: +7 });
+                        //theChannel.minDrawZoomFactX = 0.0015;
+                        theChannel.setTitle(comp.id);
+                        theChannel.setHeight(200);
+                        that.panelBrowser.addChannel(theChannel, false);
+
+                        //Attach a custom tooltip creation function to the channel
+                        theChannel.getToolTipContent = function (compID, pointIndex) {
+                            var value = this.myComponents[compID].myfetcher.getColumnPoint(pointIndex, compID);
+                            if (value != null)
+                                return that.dataFetcherSNPs.getColumnPoint(pointIndex, 'snpid') + '; ' + compID + '= ' + value.toFixed(2);
+                            else
+                                return 'No value';
+                        }
+
+                        if ('filterbankFolder' in comp) {
+                            //Filterbank summaries
+                            var summaryID = comp.filterbankID;
+                            $.each(summaryComps, function (idx, summaryComp) {
+                                var summCompID = summaryID + '_' + summaryComp.id;
+                                var colinfo = that.dataFetcherProfiles.addFetchColumn(summaryFolder + '/' + comp.filterbankFolder, 'Summ01', summCompID);
+                                var summComp = theChannel.addComponent(ChannelYVals.CompFilled(summCompID, that.dataFetcherProfiles, colinfo.myID));
+                                summComp.setColor(summaryComp.color, summaryComp.opacity);
+                                summComp.myPlotHints.makeDrawLines(3000000.0); //This causes the points to be connected with lines
+                                summComp.myPlotHints.interruptLineAtAbsent = true;
+                                summComp.myPlotHints.drawPoints = false;
+                                theChannel.modifyComponentActiveStatus(summCompID, true, false);
+                            });
+                        }
+
+                        //Individual SNP values
                         var colinfo = that.dataFetcherSNPs.addFetchColumn(comp.id, "Float2");
                         plotcomp = theChannel.addComponent(ChannelYVals.Comp(comp.id, that.dataFetcherSNPs, comp.id));
                         plotcomp.myPlotHints.color = comp.color;
                         plotcomp.myPlotHints.pointStyle = 1;
-                        theChannel.modifyComponentActiveStatus(comp.id, true);
+                        theChannel.modifyComponentActiveStatus(comp.id, false);
 
                         that.panelBrowser.channelModifyVisibility(theChannel.getID(), true);
 
