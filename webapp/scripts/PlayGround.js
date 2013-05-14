edd0f4aa1221ba2d1d02bfdeee3c733e90629b06
Paul Vauterin <pvaut@gmail.com>
2013-05-14 08:42:46 +0100

    Costmetics

--- webapp/scripts/PlayGround.js
+++ webapp/scripts/PlayGround.js
@@ -7,23 +7,27 @@
         var PlayGround = {}
 
         PlayGround.testPopup = function () {//a test function
+
+            //Initialise PopupFrame
             var popup = PopupFrame.PopupFrame('testPopupFrame', Framework.FrameGroupHor(''), { title: 'Test', sizeX: 900, sizeY: 400 });
             var frameRoot = popup.getFrameRoot();
             frameRoot.setFrameClass('DQXLight');
             frameRoot.setMarginsIndividual(0, 7, 0, 0);
 
-            var settFrame = frameRoot.addMemberFrame(Framework.FrameFinal('', 0.5)).setMargins(5).setDisplayTitle('Settings');
+            var settFrame = frameRoot.addMemberFrame(Framework.FrameFinal('', 0.5)).setMargins(5).setDisplayTitle('Settings').setFixedSize(Framework.dimX, 220);
 
             var tabpanel = frameRoot.addMemberFrame(Framework.FrameGroupTab('', 0.5)).setMarginsIndividual(0, 7, 0, 0).setDisplayTitle('Part 2').setFrameClass('DQXLight').setFrameClassClient('DQXForm');
-            tabpanel.addMemberFrame(Framework.FrameFinal('', 0.5)).setMargins(5).setDisplayTitle('Tab 1');
-            tabpanel.addMemberFrame(Framework.FrameFinal('', 0.5)).setMargins(5).setDisplayTitle('Tab 2');
+            var tab01 = tabpanel.addMemberFrame(Framework.FrameFinal('', 0.5)).setMargins(5).setDisplayTitle('Tab 1');
+            var tab02 = tabpanel.addMemberFrame(Framework.FrameFinal('', 0.5)).setMargins(5).setDisplayTitle('Tab 2');
 
-            var stackpanel = frameRoot.addMemberFrame(Framework.FrameGroupStack('', 0.5)).setMarginsIndividual(0, 7, 0, 0).setDisplayTitle('Part 3').setFrameClass('DQXLight').setFrameClassClient('DQXForm');
-            var tb01 = stackpanel.addMemberFrame(Framework.FrameFinal('tb01', 0.5)).setMargins(5);
-            var tb02 = stackpanel.addMemberFrame(Framework.FrameFinal('tb02', 0.5)).setMargins(5);
-
+            var stackpanel = frameRoot.addMemberFrame(Framework.FrameGroupStack('', 0.5)).setMargins(0).setDisplayTitle('Part 3').setFrameClass('DQXLight').setFrameClassClient('DQXForm');
+            var stc01 = stackpanel.addMemberFrame(Framework.FrameFinal('stc01', 0.5)).setMargins(5);
+            var stc02 = stackpanel.addMemberFrame(Framework.FrameFinal('stc02', 0.5)).setMargins(5);
 
             popup.render();
+
+
+            //Populate frame panels
 
             var settForm = Framework.Form(settFrame);
             var chk = Controls.Check('', { label: 'Switch', value: true });
@@ -31,7 +35,7 @@
             chk.setOnChanged(function () {
                 showhide1.setVisible(chk.getValue());
                 showhide2.setVisible(!chk.getValue());
-                stackpanel.switchTab(chk.getValue() ? 'tb01' : 'tb02');
+                stackpanel.switchTab(chk.getValue() ? 'stc01' : 'stc02');
             });
 
             var cmp1 = Controls.CompoundVert([Controls.Check('', { label: 'Check1' }), Controls.Check('', { label: 'Check2' })]);
@@ -46,8 +50,10 @@
 
             settForm.render();
 
-            tb01.setContentHtml('The content of stack component 1');
-            tb02.setContentHtml('The content of stack component 2<br/>22222222222222222222');
+            tab01.setContentHtml('The content of tab component 1');
+            tab02.setContentHtml('The content of tab component 2<br/>22222222222222222222');
+            stc01.setContentHtml('The content of stack component 1');
+            stc02.setContentHtml('The content of stack component 2<br/>22222222222222222222');
 
         }
 
